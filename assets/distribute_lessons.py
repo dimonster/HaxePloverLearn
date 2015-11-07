bf2c677419eb3d3ea04beda9cb14300de5789194
Di <didoesdigital@gmail.com>
2015-11-07 17:51:50 +1100

    Teach lessons to look for .swf in the assets directory

--- assets/distribute_lessons.py
+++ assets/distribute_lessons.py
@@ -2,8 +2,9 @@ import shutil
 import os
 
 # Take index.html and copy it to each of the lesson folders. index.html itself is only a template.
-
 for fileName in os.listdir('.'):
     if os.path.isdir(fileName):
         print fileName
         shutil.copy2('index.html', fileName + '/index.html')
+
+shutil.copy2('../Export/flash/bin/StenoPlatformer.swf', 'StenoPlatformer.swf')
