1ede5d150cdd9e9458b94574d9719c5b026d8c16
Di <didoesdigital@gmail.com>
2015-11-07 15:15:55 +1100

    Make word field wider

--- Source/ploverlearn/stenoplat/GUIMain.hx
+++ Source/ploverlearn/stenoplat/GUIMain.hx
@@ -17,9 +17,9 @@ using StringTools;
 /**
  * ...
  * @author EN
- * 
+ *
  * The main drill page.
- * 
+ *
  */
 
 class GUIMain extends Sprite
@@ -39,20 +39,20 @@ class GUIMain extends Sprite
 	private var ploverStrokes : Int;
 	private var misstrokes : Int;
 	private var fileName : String;
-	
+
 	private var metrics : Metrics;
 	private var lastKeyTime:Float = 0;
-	
+
 	private var _timer:Timer;
 	private var _deltimer:Timer;
 	private var _delarray:Array<Int>;
-	
+
 	private var started:Bool = false;
-	
+
 	//Plover usually has ~5ms delay between text events belonging to the same stroke, this is 6x that for a generous margin of error, should still be more than tight enough
 	private static inline var MAX_PLOVER_DELAY:Int = 30;
 	private static inline var BACKSPACE_KEY:Int = 8;
-	
+
 	public function new(fileName)
 	{
 		super();
@@ -66,64 +66,64 @@ class GUIMain extends Sprite
 			addEventListener(Event.ADDED_TO_STAGE, init);
 		}
 	}
-	
+
 	private function init(e : Event = null) : Void
 	{
 		var loader : LoadExercise = new LoadExercise(fileName, onExerciseLoaded);
 		this.addChild(loader);
 		loader.load();
 	}
-	
+
 	private function onExerciseLoaded(exercise : Exercise)
 	{
 		this.exercise = exercise;
-		
+
 		loadExercise();
 	}
-	
+
 	private function loadExercise(e : Event = null)
 	{
 		drawBackground();
-		
+
 		initSettingsField();
 		initWordsField();
 		initInputField();
 		initHintField();
 		initPreviewField();
 		initMetrics();
-		
+
 		inputField.addEventListener(TextEvent.TEXT_INPUT, txtListener);
 		parent.addEventListener(KeyboardEvent.KEY_DOWN, keyListener);
 		parent.addEventListener(MouseEvent.CLICK, onClick);
-		
+
 		endSplash = new SplashScreen(exercise.lessonTitle + "\nFinished!", "Click to re-start.", onHideSplash);
 		addChild(endSplash);
-		
+
 		startSplash = new SplashScreen(exercise.lessonTitle + ":\n" + exercise.exerciseName, "Click to start", onHideSplash);
 		addChild(startSplash);
 		startSplash.show();
 		nextWord();
 	}
-	
+
 	private function drawBackground()
 	{
 		graphics.beginFill(0x00000000);
 		graphics.drawRect(0, 0, stage.stageWidth, stage.stageHeight);
 		graphics.endFill();
 	}
-	
+
 	private function initWordsField()
 	{
 		var wordsFieldFormat : TextFormat = new TextFormat();
 		wordsFieldFormat.size = 40;
 		wordsFieldFormat.bold = true;
 		wordsFieldFormat.align = TextFormatAlign.CENTER;
-		
-		
+
+
 		wordsField = new TextField();
 		wordsField.textColor = 0xffffff;
 		wordsField.type = TextFieldType.DYNAMIC;
-		wordsField.text = "words go here...";
+		wordsField.text = "Just say 'hi' and see what happens!";
 		wordsField.setTextFormat(wordsFieldFormat);
 		wordsField.defaultTextFormat = wordsFieldFormat;
 		wordsField.width = wordsField.textWidth + 10;
@@ -131,39 +131,39 @@ class GUIMain extends Sprite
 		wordsField.x = (stage.stageWidth / 2) - wordsField.width / 2;
 		wordsField.y = (stage.stageHeight / 2) - wordsField.height;
 		wordsField.selectable = false;
-		
+
 		this.addChild(wordsField);
 	}
-	
+
 	private function initSettingsField()
 	{
 		settingsField = new NiceTextField("settings", 15, 0xaaaaaa, 1.0, false, stage.stageWidth);
 		settingsField.height = 200;
 		settingsField.x = (stage.stageWidth / 2) - settingsField.width / 2;
 		settingsField.y = 1 * stage.stageHeight / 8;
-		
+
 		var strs = [];
-		
+
 		if (exercise.caseSensitive)
 		{
 			strs.push("CASE SENSITIVE");
 		}
-		
+
 		if (exercise.requireSpaces)
 		{
 			strs.push("SPACES REQUIRED");
 		}
-		
+
 		if (exercise.noticeSpaces)
 		{
 			strs.push("SPACES NOT REMOVED");
 		}
-		
+
 		if (exercise.ignoredChars != null)
 		{
 			strs.push("IGNORING: (" + exercise.ignoredChars.join(",")+")");
 		}
-		
+
 		if (strs.length != 0)
 		{
 			settingsField.text = strs.join(", ");
@@ -172,33 +172,33 @@ class GUIMain extends Sprite
 		{
 			settingsField.text = "";
 		}
-		
+
 		this.addChild(settingsField);
 	}
-	
+
 	private function initInputField()
 	{
 		var inputFieldFormat : TextFormat = new TextFormat();
 		inputFieldFormat.size = 20;
 		inputFieldFormat.bold = true;
 		inputFieldFormat.align = TextFormatAlign.CENTER;
-		
+
 		inputField = new TextField();
 		inputField.setTextFormat(inputFieldFormat);
 		inputField.defaultTextFormat = inputFieldFormat;
 		inputField.textColor = 0xaaaaaa;
 		inputField.type = TextFieldType.INPUT;
 		inputField.border = true;
-		inputField.text = "even more words go here";
+		inputField.text = "Just say 'hi' and see what happens!";
 		inputField.width = inputField.textWidth + 5;
 		inputField.height = inputField.textHeight + 3;
 		inputField.x = (stage.stageWidth / 2) - inputField.width / 2;
 		inputField.y = (stage.stageHeight / 2) + inputField.height;
-		
+
 		this.addChild(inputField);
 		stage.focus = inputField;
 	}
-	
+
 	private function initPreviewField()
 	{
 		var widthLeft = (stage.stageWidth - wordsField.width) / 2;
@@ -208,7 +208,7 @@ class GUIMain extends Sprite
 		this.addChild(previewField);
 		previewField.visible = false;
 	}
-	
+
 	private function initHintField()
 	{
 		hintField = new NiceTextField("hint?", 20, 0xaaaaaa, 1.0, false, stage.stageWidth);
@@ -217,12 +217,12 @@ class GUIMain extends Sprite
 		hintField.addEventListener(MouseEvent.CLICK, onHintClick);
 		this.addChild(hintField);
 	}
-	
+
 	private function initMetrics()
 	{
 		metrics = new Metrics();
 	}
-	
+
 	private function wordEndsIn(word:String,arr:Array<String>):Bool
 	{
 		var end = word.length - 1;
@@ -235,12 +235,12 @@ class GUIMain extends Sprite
 		}
 		return false;
 	}
-	
+
 	private function keyListener(e : KeyboardEvent)
 	{
 		if (_delarray == null) _delarray = [];
 		_delarray.push(e.keyCode);
-		
+
 		if (_deltimer != null)
 		{
 			_deltimer.stop();
@@ -251,24 +251,24 @@ class GUIMain extends Sprite
 			onCheckDelete();
 		}
 	}
-	
+
 	private function onCheckDelete()
 	{
 		var oneDelete:Bool = false;
 		var oneNormal:Bool = false;
-		
+
 		var allDelete:Bool = false;
 		var isTransform:Bool = false;
-		
+
 		for (key in _delarray)
 		{
 			if (key != BACKSPACE_KEY) oneNormal = true;
 			else oneDelete = true;
 		}
-		
+
 		allDelete = oneDelete && !oneNormal;
 		isTransform = oneDelete && oneNormal;
-		
+
 		if (allDelete)
 		{
 			onDelete();
@@ -277,51 +277,51 @@ class GUIMain extends Sprite
 		{
 			onTransform();
 		}
-		
+
 		_deltimer.stop();
 		_delarray = [];
 	}
-	
+
 	private function onDelete():Void
 	{
 		//if I want to do something here
 	}
-	
+
 	private function onTransform():Void
 	{
 		metrics.forgive();
 	}
-	
+
 	private function onStart():Void
 	{
 		started = true;
 		metrics.startTime();
 		previewField.visible = true;
 	}
-	
+
 	private function txtListener(e : TextEvent)
 	{
 		if (!started)
 		{
 			onStart();
 		}
-		
+
 		var time = Lib.getTimer();
 		var elapsed = time - lastKeyTime;
 		lastKeyTime = time;
-		
+
 		lettersTyped++;
-		
+
 		if (_timer != null){ _timer.stop(); }
 		_timer = new Timer(MAX_PLOVER_DELAY);
-		
+
 		//set a timer, and if it runs out before the next text input event, count that as the end of the Plover stroke
-		_timer.run = function () 
+		_timer.run = function ()
 		{
 			onInputText(e);
 		}
 	}
-	
+
 	private function cleanField(field:TextField):Void
 	{
 		if (exercise.ignoredChars != null)
@@ -335,7 +335,7 @@ class GUIMain extends Sprite
 			}
 		}
 	}
-	
+
 	private function cleanInput(inStr:String,targStr:String):{input:String,target:String}
 	{
 		if (!exercise.caseSensitive)
@@ -343,14 +343,14 @@ class GUIMain extends Sprite
 			inStr = inStr.toLowerCase();
 			targStr = targStr.toLowerCase();
 		}
-		
+
 		//Plover sometimes inserts spaces between words
 		//If there's a space at the BEGINNING of user input, it is always deleted
 		while (inStr.indexOf(" ") == 0)
 		{
 			inStr = inStr.substr(1, inStr.length - 1);
 		}
-		
+
 		//If noticeSpaces is on, don't do anything to the spaces
 		if (!exercise.noticeSpaces)
 		{
@@ -366,7 +366,7 @@ class GUIMain extends Sprite
 				targStr = targStr + " ";
 			}
 		}
-		
+
 		if (exercise.ignoredChars != null)
 		{
 			for (char in exercise.ignoredChars)
@@ -381,20 +381,20 @@ class GUIMain extends Sprite
 				}
 			}
 		}
-		
+
 		return { input:inStr, target:targStr };
 	}
-	
+
 	private function onInputText(e: TextEvent):Void
 	{
 		ploverStrokes++;
-		
+
 		var clean = cleanInput(inputField.text, wordsField.text);
 		var inStr = clean.input;
 		var targStr = clean.target;
-		
+
 		cleanField(inputField);
-		
+
 		if (inStr == targStr)
 		{
 			nextWord();
@@ -408,7 +408,7 @@ class GUIMain extends Sprite
 			misstrokes++;
 			metrics.logMisstroke();
 			metrics.logStreak(false);
-			
+
 			//TODO: watch out for edge cases where Plover goes back and "corrects" previous strokes in multi-stroke words, these would be false positives
 			//I think a heuristic that watches to see if you are able to complete the word in say, 4 strokes or less, without ever using the delete stroke,
 			//should be able to infer that "all's well that ends well", and not count any of that as a misstroke.
@@ -419,12 +419,12 @@ class GUIMain extends Sprite
 		}
 		_timer.stop();
 	}
-	
+
 	private function nextWord()
 	{
 		metrics.logWord(wordsField.text);
 		metrics.logStreak(true);
-		
+
 		if (exercise.hasNextWord())
 		{
 			exercise.nextWord();
@@ -432,29 +432,29 @@ class GUIMain extends Sprite
 			previewField.text = exercise.peekWord(1) + "    " + exercise.peekWord(2) + "    " + exercise.peekWord(3);
 			hintField.text = "hint?";
 		}
-		else 
+		else
 		{
 			finish();
 		}
-		
+
 		inputField.text = "";
 		hintField.visible = false;
 		lettersTyped = 0;
 		ploverStrokes = 0;
 		misstrokes = 0;
 	}
-	
+
 	private function finish()
 	{
 		metrics.stopTime();
 		endSplash.show(metrics);
 	}
-	
+
 	private function onHideSplash()
 	{
 		reset();
 	}
-	
+
 	private function reset()
 	{
 		metrics.reset();
@@ -464,12 +464,12 @@ class GUIMain extends Sprite
 		inputField.text = "";
 		started = false;
 	}
-	
+
 	private function onClick(e : Event)
 	{
 		stage.focus = inputField;
 	}
-	
+
 	private function onHintClick(e : Event)
 	{
 		hintField.text = exercise.hint();
