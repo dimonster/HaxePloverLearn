304fc633c56d75ebbd42c8b8ff5898cd18e12754
Di <didoesdigital@gmail.com>
2015-11-14 22:00:56 +1100

    Add reference panel content html

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,12 @@ $(function() {
   $('#show-reference-panel').change(function(){
     $('#js-reference-panel').toggleClass("hide",!this.checked);
   });
+
+  $.get('../reference-panel.html',function(data){
+    var $referenceContent = $(data).find('#lesson5a-prefixes');
+    console.log($referenceContent);
+    $('#js-reference-panel').html($referenceContent);
+  });
+
 });
 
