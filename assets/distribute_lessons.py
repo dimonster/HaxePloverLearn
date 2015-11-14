bea03268d0aae916020214a0b8e3af16e5a6d6a6
Di <didoesdigital@gmail.com>
2015-11-14 15:07:00 +1100

    Move lessons into /lessons

--- assets/distribute_lessons.py
+++ assets/distribute_lessons.py
@@ -2,7 +2,7 @@ import shutil
 import os
 
 # Take index.html and copy it to each of the lesson folders. index.html itself is only a template.
-for fileName in os.listdir('.'):
+for fileName in os.listdir('lessons'):
     if os.path.isdir(fileName):
         print fileName
         shutil.copy2('index.html', fileName + '/index.html')
