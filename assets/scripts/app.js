3aeb606661b695a3ad3d24042effb720d91a9316
Di <didoesdigital+steno@gmail.com>
2017-04-25 16:28:35 +1000

    Fetch local reference panel
    TODO: clean up redundant mess of function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,30 @@
 $(function() {
+
+  // Get reference panel content from global reference panel
   $.get('/lessons/reference-panel.html',function(data){
     var path = location.pathname.toLowerCase().split('/');
     var lessonid = path[path.length-2];  // eg "punctuation-in-practice"
 
     if (lessonid) {
       var $referenceContent = $(data).find('#'+lessonid).children();
+
+      if ($referenceContent.length > 0) {
+        // fill content
+        $('#js-reference-panel-content').html($referenceContent);
+
+        // show reference panel
+        $('#js-reference-panel').removeClass('hide');
+      }
+    }
+  });
+
+  // Get reference panel content from current folder
+  $.get('./lesson-reference-panel.html',function(data){
+    var path = location.pathname.toLowerCase().split('/');
+    var lessonid = path[path.length-2];  // eg "punctuation-in-practice"
+
+    if (lessonid) {
+      var $referenceContent = $(data).children();
 
       if ($referenceContent.length > 0) {
         // fill content
